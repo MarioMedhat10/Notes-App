d7b6a6658fb249ca6f7e031ec098fcc84a45711b
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-01-12 01:09:46 +0200

    create NotesViewBody

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: 40,
+          right: 24,
+          left: 24,
+        ),
+        child: NotesViewBody(),
+      ),
+    );
   }
 }
