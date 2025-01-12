2698357ca47e7f0e17ef1e7867786181d4371dfb
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-01-12 04:19:50 +0200

    create NoteBottomSheet

---
+++ lib/views/widgets/note_bottom_sheet.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
+
+class NoteBottomSheet extends StatelessWidget {
+  const NoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 32,
+        horizontal: 16,
+      ),
+      child: Column(
+        children: [
+          CustomTextField(
+            hintText: 'Title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hintText: 'Content',
+            maxLines: 5,
+          ),
+        ],
+      ),
+    );
+  }
+}
