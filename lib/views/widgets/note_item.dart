0853c8bf41a8fa77089410d346bab97f213f2628
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-01-12 02:50:08 +0200

    create NoteItem

---
+++ lib/views/widgets/note_item.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 24,
+        bottom: 24,
+        left: 16,
+      ),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFCD81),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter tips',
+              style: TextStyle(
+                fontSize: 26,
+                color: Colors.black,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16,
+              ),
+              child: Text(
+                'Build your Career with Mario Medhat',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black.withAlpha(120),
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 24,
+            ),
+            child: Text(
+              'May 21,2022',
+              style: TextStyle(
+                color: Colors.black.withAlpha(120),
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -9,6 +10,10 @@ class NotesViewBody extends StatelessWidget {
     return Column(
       children: [
         CustomAppBar(),
+        SizedBox(
+          height: 20,
+        ),
+        NoteItem(),
       ],
     );
   }
