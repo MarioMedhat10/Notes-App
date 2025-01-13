63a9ccc84cb7583eadae3aa6689c8d3d82900663
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-01-13 05:47:46 +0200

    create EditNoteView and EditNoteViewBody

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({
@@ -8,62 +9,72 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: const Color(0xFFFFCD81),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter tips',
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.black,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 16,
-              ),
-              child: Text(
-                'Build your Career with Mario Medhat',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const EditNoteView(),
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFCD81),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter tips',
                 style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black.withAlpha(120),
+                  fontSize: 26,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                ),
+                child: Text(
+                  'Build your Career with Mario Medhat',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withAlpha(120),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 24,
+              ),
+              child: Text(
+                'May 21,2022',
+                style: TextStyle(
+                  color: Colors.black.withAlpha(120),
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              right: 24,
-            ),
-            child: Text(
-              'May 21,2022',
-              style: TextStyle(
-                color: Colors.black.withAlpha(120),
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
