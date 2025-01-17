a52d4287daf205152074b6fe846e098f16cda61d
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-01-17 21:32:50 +0200

    use AbsorbPointer while loading state

--- lib/views/widgets/note_bottom_sheet.dart
+++ lib/views/widgets/note_bottom_sheet.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
 
@@ -13,28 +12,28 @@ class NoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AddNoteCubit(),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 32,
-          horizontal: 16,
-        ),
-        child: BlocConsumer<AddNoteCubit, AddNoteState>(
-          listener: (context, state) {
-            if (state is AddNoteFailureState) {
-              log('failed ${state.errMessage}');
-            } else if (state is AddNoteSuccessState) {
-              Navigator.pop(context);
-            }
-          },
-          builder: (context, state) {
-            return ModalProgressHUD(
-              inAsyncCall: state is AddNoteLoadingState ? true : false,
-              child: const SingleChildScrollView(
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteFailureState) {
+            log('failed ${state.errMessage}');
+          } else if (state is AddNoteSuccessState) {
+            Navigator.pop(context);
+          }
+        },
+        builder: (context, state) {
+          return AbsorbPointer(
+            absorbing: state is AddNoteLoadingState ? true : false,
+            child: const Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 32,
+                horizontal: 16,
+              ),
+              child: SingleChildScrollView(
                 child: AddNoteForm(),
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
