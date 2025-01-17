193b3de185691a00ced4ad1a4547b942acdc4cc7
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-01-17 20:48:11 +0200

    consume AddNoteCubit and use addNote method in CustomButton

--- lib/views/widgets/note_bottom_sheet.dart
+++ lib/views/widgets/note_bottom_sheet.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
 
 class NoteBottomSheet extends StatelessWidget {
@@ -6,13 +11,27 @@ class NoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Padding(
-      padding: EdgeInsets.symmetric(
+    return Padding(
+      padding: const EdgeInsets.symmetric(
         vertical: 32,
         horizontal: 16,
       ),
-      child: SingleChildScrollView(
-        child: AddNoteForm(),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteFailureState) {
+            log('failed ${state.errMessage}');
+          } else if (state is AddNoteSuccessState) {
+            Navigator.pop(context);
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoadingState ? true : false,
+            child: const SingleChildScrollView(
+              child: AddNoteForm(),
+            ),
+          );
+        },
       ),
     );
   }
